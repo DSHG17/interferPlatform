7d708f742fde96cc5527d0c956bc7b20380f38ba
DSHG17 <tohsakaringod@gmail.com>
2025-02-24 17:00:46 -0600

    feat: added the default user function to add the only admin

---
+++ src/user/user.controller.js
@@ -0,0 +1,30 @@
+import { uploadProfilePicture } from "../middlewares/multer-uploads.js";
+import User from "./user.model.js"
+import { hash } from "argon2";
+
+export  const defaultUser = async () =>{
+
+    uploadProfilePicture.single()
+    const adminUser = {
+        "name": "Derian",
+        "surname": "Hernández",
+        "username": "Elwazawaza",
+        "role": "ADMIN_ROLE",
+        "email": "dshgonzalez11@gmail.com",
+        "password": "Samedirection14*",
+        "phone": "40191129",
+        "profile-picture": "../../public/uploads/profile-pictures/chichismit.png"
+    }
+
+    const firstUser = await User.findOne({
+        $or: [
+            { email: adminUser.email },
+            { username: adminUser.username }
+        ]
+    });
+
+    if (!firstUser) {
+        adminUser.password = await hash(adminUser.password)
+         User.create(adminUser)
+    }
+}
