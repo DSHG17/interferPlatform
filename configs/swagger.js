063ff56b903068ce6057d2f8fde8325e16db7e8c
DSHG17 <tohsakaringod@gmail.com>
2025-02-24 15:58:28 -0600

    build: initialized the project with all the basic structure

---
+++ configs/swagger.js
@@ -0,0 +1,28 @@
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express"
+
+const swaggerOptions = {
+    swaggerDefinition:{
+        openapi: "3.0.0",
+        info:{
+            title: "Interfex Platform API",
+            version:"1.0.0",
+            description: "API form the management of the Interfex Platform",
+            contact:{
+                name: "Derian Hernández",
+                email: "dhernandez-2023346@kinal.edu.gt"
+            }
+        },
+        servers:[
+            {
+                url: "http://127.0.0.1:3000/interfexPlatform/v1"
+            }
+        ]
+    },
+    apis:[
+    ]
+}
+
+const swaggerDocs = swaggerJSDoc(swaggerOptions)
+
+export { swaggerDocs, swaggerUi }
